60e3a870bd684ec95d863b057133357b34cec59d
Vasiliev Evgenii <eugene.unn@gmail.com>
2022-08-08 01:44:48 +0300

    add mito color parametrization

--- Synthetics python/settings.py
+++ Synthetics python/settings.py
@@ -2,10 +2,23 @@ import random
 
 
 PARAM = {
+    'axon_shell_color_mean': 65.0,
+    'axon_shell_color_std': 5.0,
+
     'membrane_thickness_mean': 3.0,
     'membrane_thickness_std': 1.0,
     'membrane_color_mean': 80.0,
-    'membrane_color_std': 5.0,}
+    'membrane_color_std': 5.0,
+    
+    'mitohondrion_shell_color_mean': 85.0,
+    'mitohondrion_shell_color_std': 5.0,
+    'mitohondrion_back_color_mean': 120.0,
+    'mitohondrion_back_color_std': 20.0,
+    'mitohondrion_cristae_shell_color_mean': 80.0,
+    'mitohondrion_cristae_shell_color_std': 0.0,
+    'mitohondrion_cristae_color_mean': 120.0,
+    'mitohondrion_cristae_color_std': 5.0,
+}
 
 
 def uniform_float(mean, std):
