added organelle copy function fixed rotation function

Synthetics python/src/organells/PSD.py
@@ -93,6 +93,27 @@ class PSD:
 
         self.setRandomAngle(0,360)
 
+    def __copy__(self):
+        new_psd = PSD()
+        new_psd.color = self.color
+        new_psd.addColor = self.addColor
+        new_psd.centerline_color = self.centerline_color
+        new_psd.centerPoint = self.centerPoint.copy()
+        new_psd.Points = self.Points.copy()
+        new_psd.lenPSD = self.lenPSD
+        new_psd.typeGen = self.typeGen
+        new_psd.angle = self.angle
+        new_psd.mainUpSizeLinePSD   = self.mainUpSizeLinePSD
+        new_psd.inputSizeLinePSD    = self.inputSizeLinePSD
+        new_psd.mainDownSizeLinePSD = self.mainDownSizeLinePSD
+
+        new_psd.setDrawParam()
+        new_psd.setRandomAngle(0,0)
+        return new_psd
+
+    def copy(self):
+        return self.__copy__()
+
     def CreateTreePoints(self, TreePoints):
         # в 1 из 10 случаев полностью черная, в 9/10 с полосой внутри
         if np.random.randint(0,10) == 0:
@@ -191,9 +212,9 @@ class PSD:
         else:
             sign = 1
 
-        self.angle = (self.angle + np.random.randint(min_angle, max_angle+1) * sign) %360
-
-        change_angle = self.angle * (math.pi/180)
+        new_angle = (self.angle + np.random.randint(min_angle, max_angle+1) * sign) %360
+        change_angle = (new_angle - self.angle) * (math.pi/180)
+        self.angle = new_angle
 
         tPoints = []
         for point in self.Points:

Synthetics python/src/organells/axon.py
@@ -43,14 +43,42 @@ class Axon:
         self.nowAddPen = Pen(self.shell_color, self.sizeAddPen)
         self.Create()
 
+    def __copy__(self):
+        new_axon = Axon()
+
+        if self.innerTexture is not None:
+            new_axon.nowInnerBrush = self.nowInnerBrush.copy()
+            new_axon.nowBubbleBrush= self.nowBubbleBrush.copy()
+            new_axon.innerTexture  = self.innerTexture.copy()
+            new_axon.bubbleTexture = self.bubbleTexture.copy()
+
+        new_axon.shell_color = self.shell_color
+        new_axon.sizePen = self.sizePen
+        self.sizeAddPen = self.sizeAddPen
+        new_axon.centerPoint = self.centerPoint.copy()
+        new_axon.input_radius = self.input_radius
+        new_axon.angle = self.angle
+        new_axon.sublistNumber = self.sublistNumber
+        new_axon.pointNumber = self.pointNumber
+        new_axon.Points = self.Points.copy()
+        new_axon.typeGen = self.typeGen
+
+        new_axon.nowPen = self.nowPen.copy()
+        new_axon.nowAddPen = self.nowAddPen.copy()
+
+        new_axon.setDrawParam()
+        new_axon.setRandomAngle(0,0)
+        return new_axon
+
+    def copy(self):
+        return self.__copy__()
+
     def Create(self, min_r=0, max_r=0):
 
         if np.random.randint(0, 5) == 0:
             self.typeGen = 0
         else:
             self.typeGen = 1
-        
-        
 
         self.Points = []
 
@@ -142,9 +170,9 @@ class Axon:
         else:
             sign = 1
 
-        self.angle = (self.angle + np.random.randint(min_angle, max_angle+1) * sign) %360
-
-        change_angle = self.angle * (math.pi/180)
+        new_angle = (self.angle + np.random.randint(min_angle, max_angle+1) * sign) %360
+        change_angle = (new_angle - self.angle) * (math.pi/180)
+        self.angle = new_angle
 
         tPoints = []
         for point in self.Points:

Synthetics python/src/organells/mitohondrion.py
@@ -38,12 +38,42 @@ class Mitohondrion:
         self.PointsWithOffset = []
 
         self.direction = [0,0]         # вектор смещения границы, для имитации косоро среза
-
+        self.dopSizeLine = 0
 
         self.addPoints = []            # точки с смещением для имитации косоро среза
         self.addPointsWithOffset = []
 
         self.Create()
+
+    def __copy__(self):
+        new_mito = Mitohondrion()
+
+        new_mito.color = self.color
+        new_mito.addColor = self.addColor
+        new_mito.nowPen  = self.nowPen.copy()
+
+        if self.texture is not None:
+            new_mito.nowBrush  = self.nowBrush.copy()
+            new_mito.texture  = self.texture.copy()
+
+        new_mito.angle = self.angle
+        new_mito.main_len = self.main_len
+        new_mito.numberPoints = self.numberPoints
+        new_mito.centerPoint = self.centerPoint.copy()
+        new_mito.Points = self.Points.copy()
+
+        new_mito.direction  = self.direction.copy()
+        new_mito.addPoints  = self.addPoints.copy()
+
+        new_mito.dopSizeLine = self.dopSizeLine
+
+        new_mito.setDrawParam()
+        new_mito.setRandomAngle(0,0)
+
+        return new_mito
+
+    def copy(self):
+        return self.__copy__()
 
     def Create(self):
 
@@ -100,7 +130,7 @@ class Mitohondrion:
         self.Points = tPoints
 
         #добавление доп. точек
-        if np.random.random() < 0.5 or True: ##############################
+        if np.random.random() < 0.5: ##############################
             self.dopSizeLine = np.random.randint(3,4+1)
 
             direction = np.random.randint(-4,4+1, 2)
@@ -163,9 +193,9 @@ class Mitohondrion:
         else:
             sign = 1
 
-        self.angle = (self.angle + np.random.randint(min_angle, max_angle+1) * sign) %360
-
-        change_angle = self.angle * (math.pi/180)
+        new_angle = (self.angle + np.random.randint(min_angle, max_angle+1) * sign) %360
+        change_angle = (new_angle - self.angle) * (math.pi/180)
+        self.angle = new_angle
 
         tPoints = []
         for point in self.Points:
@@ -256,8 +286,7 @@ class Mitohondrion:
 
         self.nowBrush.FullBrush(draw_image, self.PointsWithOffset)
 
-        if self.direction[0] != 0 and self.direction[1] != 0:
-
+        if self.dopSizeLine != 0 and  self.direction[0] != 0 and self.direction[1] != 0:
             for step in range(-self.dopSizeLine, self.dopSizeLine+1, self.nowPen.sizePen-1):
 
                 temp_PointsWithOffset_for_boarder = []

Synthetics python/src/organells/vesicles.py
@@ -37,9 +37,38 @@ class Vesicles:
         self.Points = []
         self.PointsWithOffset = []
 
+        self.varibleInputFulling = 0
+
         self.typeGen = 0            # 0 - norma mode, 1 - many small vesicles
 
         self.Create()
+
+    def __copy__(self):
+        new_vesic = Vesicles()
+
+        new_vesic.sizeVesiculeMin = self.sizeVesiculeMin
+        new_vesic.sizeVesiculeMax = self.sizeVesiculeMax
+        new_vesic.numberPoints = self.numberPoints
+        new_vesic.listSizeVesiculs = self.listSizeVesiculs
+
+        new_vesic.color = self.color
+        new_vesic.nowPen  = self.nowPen.copy()
+        new_vesic.addColor = self.addColor
+
+        new_vesic.centerPoint = self.centerPoint.copy()
+        new_vesic.Points = self.Points.copy()
+
+        new_vesic.angle = self.angle
+        new_vesic.typeGen = self.typeGen
+
+        new_vesic.varibleInputFulling = self.varibleInputFulling
+
+        new_vesic.setDrawParam()
+        new_vesic.setRandomAngle(0,0)
+        return new_vesic
+
+    def copy(self):
+        return self.__copy__()
 
     def Create(self):
 
@@ -123,9 +152,9 @@ class Vesicles:
         else:
             sign = 1
 
-        self.angle = (self.angle + np.random.randint(min_angle, max_angle+1) * sign) %360
-
-        change_angle = self.angle * (math.pi/180)
+        new_angle = (self.angle + np.random.randint(min_angle, max_angle+1) * sign) %360
+        change_angle = (new_angle - self.angle) * (math.pi/180)
+        self.angle = new_angle
 
         tPoints = []
         for point in self.Points: