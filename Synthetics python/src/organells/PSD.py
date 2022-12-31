fixed calculation of PSD angle

Synthetics python/src/organells/PSD.py
@@ -147,9 +147,12 @@ class PSD:
         vec = [2*vec[0]/self.lenPSD, 2*vec[1]/self.lenPSD]
 
         if vec[0] != 0:
-            self.angle = math.atan(vec[1]/vec[0]) * 180 / math.pi
+            #print("\t\ttype 1")
+            self.angle = math.degrees(math.atan(vec[1]/vec[0]))
         else:
-            self.angle = math.copysign(1, vec[1]) * 90
+            #print("\t\ttype 2")
+            self.angle = math.degrees(math.pi/2 - math.atan(vec[0]/vec[1]))
+
         normal_y = math.sqrt((self.centerPoint[0] - normal[0])**2 + (self.centerPoint[1] - normal[1])**2)
 
         ########### 0-2 main-line (цвет PSD или цвет межкеточный)
@@ -197,7 +200,7 @@ class PSD:
         self.Points.append([self.Points[7][0], int(self.Points[7][1] + sizeOffset)])
         self.Points.append([self.Points[6][0] , int(self.Points[6][1] + sizeOffset2)])
 
-        self.setRandomAngle(0,0)
+        self.setAngle(self.angle)
 
     def ChangePositionPoints(self):
         self.PointsWithOffset = []
@@ -205,16 +208,8 @@ class PSD:
         for point in self.Points:
             self.PointsWithOffset.append([self.centerPoint[0]+point[0], self.centerPoint[1]+point[1]])
 
-    def setRandomAngle(self, min_angle = 0, max_angle = 90, is_singned_change = True):
-
-        if np.random.random() < 0.5 and is_singned_change:
-            sign = -1
-        else:
-            sign = 1
-
-        new_angle = (self.angle + np.random.randint(min_angle, max_angle+1) * sign) %360
-        change_angle = (new_angle - self.angle) * (math.pi/180)
-        self.angle = new_angle
+    def setAngle(self, change_angle):
+        change_angle = math.radians(change_angle)
 
         tPoints = []
         for point in self.Points:
@@ -224,6 +219,19 @@ class PSD:
 
         self.Points = tPoints
         self.ChangePositionPoints()
+
+    def setRandomAngle(self, min_angle = 0, max_angle = 90, is_singned_change = True):
+
+        if np.random.random() < 0.5 and is_singned_change:
+            sign = -1
+        else:
+            sign = 1
+
+        new_angle = (self.angle + np.random.randint(min_angle, max_angle+1) * sign) %360
+        change_angle = (new_angle - self.angle)
+        self.angle = new_angle
+
+        self.setAngle(change_angle)
 
 
     def setDrawParam(self):
@@ -242,7 +250,32 @@ class PSD:
 
         self.ChangePositionPoints()
 
-    def Draw(self, image, layer_drawing = True):
+    def AddedBlur(self, draw_image, rangeList):
+        arr = np.asarray(rangeList)
+        offset = 20
+        minxy = np.amin(arr, axis=0)
+        minxy = np.maximum(np.zeros(2), minxy - offset).astype(np.int)
+        maxxy = np.amax(arr, axis=0)
+        maxxy = np.minimum(np.asarray([draw_image.shape[0], draw_image.shape[1]]),maxxy + offset).astype(np.int)
+        add = np.random.randint(2,3)
+        r = PARAM["main_radius_gausse_blur"] + add
+        G = PARAM["main_sigma_gausse_blur"] + add + 0.5
+        patch = cv2.GaussianBlur(draw_image[minxy[1]:maxxy[1], minxy[0]:maxxy[0]],(r*2+1,r*2+1), G)
+        original = draw_image[minxy[1]:maxxy[1], minxy[0]:maxxy[0]]
+
+        self.nowBrush.brush = (255,255,255)
+        mask = np.zeros(draw_image.shape)
+        self.nowBrush.FullBrush(mask, rangeList)
+        self.nowBrush.brush = self.addColor
+        mask = mask[minxy[1]:maxxy[1], minxy[0]:maxxy[0],0]
+
+        kernel = np.ones((r, r), np.uint8)
+        mask = cv2.dilate(mask, kernel, iterations=1)
+        mask = np.repeat(mask[:, :, np.newaxis], 3, axis=2)
+        original = np.where(mask > 0, patch, original)
+        draw_image[minxy[1]:maxxy[1], minxy[0]:maxxy[0]] = original
+
+    def Draw(self, image):
         # Основная рисующая фукция
 
         draw_image = image.copy()
@@ -253,30 +286,8 @@ class PSD:
         rangeList = self.PointsWithOffset[9:9+6]
         self.nowBrush.FullBrush(draw_image, rangeList)
 
-        if layer_drawing:
-            arr = np.asarray(rangeList)
-            offset = 20
-            minxy = np.amin(arr, axis=0)
-            minxy = np.maximum(np.zeros(2), minxy - offset).astype(np.int)
-            maxxy = np.amax(arr, axis=0)
-            maxxy = np.minimum(np.asarray([draw_image.shape[0], draw_image.shape[1]]),maxxy + offset).astype(np.int)
-            add = np.random.randint(2,3)
-            r = PARAM["main_radius_gausse_blur"] + add
-            G = PARAM["main_sigma_gausse_blur"] + add + 0.5
-            patch = cv2.GaussianBlur(draw_image[minxy[1]:maxxy[1], minxy[0]:maxxy[0]],(r*2+1,r*2+1), G)
-            original = draw_image[minxy[1]:maxxy[1], minxy[0]:maxxy[0]]
-
-            self.nowBrush.brush = (255,255,255)
-            mask = np.zeros(draw_image.shape)
-            self.nowBrush.FullBrush(mask, rangeList)
-            self.nowBrush.brush = self.addColor
-            mask = mask[minxy[1]:maxxy[1], minxy[0]:maxxy[0],0]
-
-            kernel = np.ones((r, r), np.uint8)
-            mask = cv2.dilate(mask, kernel, iterations=1)
-            mask = np.repeat(mask[:, :, np.newaxis], 3, axis=2)
-            original = np.where(mask > 0, patch, original)
-            draw_image[minxy[1]:maxxy[1], minxy[0]:maxxy[0]] = original
+        #if draw_layer:
+        self.AddedBlur(draw_image, rangeList)
 
         # рисование нижней линии 
         rangeList_up = self.PointsWithOffset[3: 3+3]
@@ -289,6 +300,10 @@ class PSD:
         rangeList_input = self.PointsWithOffset[0: 0+3]
         small_spline_line(draw_image, rangeList_input, self.nowAddPen.color, self.inputSizeLinePSD)
 
+        return draw_image
+
+    def DrawLayer(self, image):
+        draw_image = self.Draw(image)
         return draw_image
 
     def DrawUniqueArea(self, image, small_mode = False):
@@ -305,7 +320,7 @@ class PSD:
             kernel = np.array([[0, 1, 0],
                                [1, 1, 1],
                                [0, 1, 0]], dtype=np.uint8)
-            draw_image = cv2.dilate(draw_image,kernel,iterations = 4)
+            draw_image = cv2.dilate(draw_image,kernel,iterations = 5)
         else:
             kernel = np.array([[0, 1, 0],
                                [1, 1, 1],
@@ -324,7 +339,11 @@ class PSD:
     def DrawMask(self, image):
         #Смена цветов для рисования маски
         self.setMaskParam()
-        mask = self.Draw(image, layer_drawing = False)
+        mask = self.Draw(image)
+
+        mask[mask < 192] = 0
+        mask[mask > 192] = 255
+
         self.setDrawParam()
 
         return mask
@@ -366,6 +385,20 @@ def testPSD():
         cv2.imshow("mask", mask)
         cv2.imshow("img2", img2)
         cv2.imshow("tecnicalMask", tecnicalMask)
+
+        r = PARAM["main_radius_gausse_blur"]
+        G = PARAM["main_sigma_gausse_blur"]
+        Img = cv2.GaussianBlur(img1,(r*2+1,r*2+1), G)
+
+        noisy = np.ones(img1.shape[:2], np.uint8)
+        noisy = np.random.poisson(noisy)*PARAM['pearson_noise'] - PARAM['pearson_noise']/2
+
+        Img = Img + cv2.merge([noisy, noisy, noisy])
+        Img[Img < 0] = 0
+        Img[Img > 255] = 255
+        Img = Img.astype(np.uint8)
+
+        cv2.imshow("add noise and blur", Img)
 
         q = cv2.waitKey()
 
@@ -411,8 +444,9 @@ def testPSD():
         print("PointsWithOffset", psd.PointsWithOffset)
 
         print("typeGen", psd.typeGen)
+        print("angle", psd.angle)
 
-        img1 = psd.Draw(img)
+        img1 = psd.DrawLayer(img)
 
         for point in tree_gen:
             cv2.circle(img1, point, 3, (0, 0, 255), 2)
@@ -426,13 +460,27 @@ def testPSD():
         tecnicalMask = np.zeros((512,512,3), np.uint8)
 
         mask = psd.DrawMask(mask)
-        img2 = psd.Draw(img)
+        img2 = psd.DrawLayer(img)
         tecnicalMask = psd.DrawUniqueArea(tecnicalMask)
 
         cv2.imshow("img", img1)
         cv2.imshow("mask", mask)
         cv2.imshow("img2", img2)
         cv2.imshow("tecnicalMask", tecnicalMask)
+
+        r = PARAM["main_radius_gausse_blur"]
+        G = PARAM["main_sigma_gausse_blur"]
+        Img = cv2.GaussianBlur(img1,(r*2+1,r*2+1), G)
+
+        noisy = np.ones(img1.shape[:2], np.uint8)
+        noisy = np.random.poisson(noisy)*PARAM['pearson_noise'] - PARAM['pearson_noise']/2
+
+        Img = Img + cv2.merge([noisy, noisy, noisy])
+        Img[Img < 0] = 0
+        Img[Img > 255] = 255
+        Img = Img.astype(np.uint8)
+
+        cv2.imshow("add noise and blur", Img)
 
         q = cv2.waitKey()
 