b281c7142ec4cc0616244367932c447487217fc3
Belokamenskaya <alexandra.belokamenskaya@gmail.com>
2023-03-10 01:53:13 +0300

    Rewrite adding elements high level function

--- Synthetics python/src/container/main_field.py
+++ Synthetics python/src/container/main_field.py
@@ -5,6 +5,7 @@ import datetime
 import math
 from settings import PARAM, uniform_int
 import skimage
+import random
 
 import albumentations as albu
 
@@ -153,249 +154,170 @@ class Form:
         draw_image = p.Draw(draw_image)
 
         return draw_image
+    
+    def get_count(self, max_count):
+        return np.random.randint(1, max_count + 1) if max_count > 0 else 0
+    
+    class_constructors = {
+        "Axon": Axon,
+        "Mitohondrion": Mitohondrion,
+        "Vesicles": Vesicles,
+        "PSD": PSD,
+    }
+    
+    def generate_lists(self, element_dict, split = True):
+        '''
+        Generate list or lists of compartment which will be used as order to adding 
+        this compartment into layer
+
+        Parameters
+        ----------
+        element_dict : dictionary
+            key: compartment type as string
+            value: number of compartment in layer.
+        split : bool
+            Need we 2 lists or one. The default is True.
+
+        Returns
+        -------
+        list
+            List of lists of compartment order.
+            For example: [["PSD","Axon", "PSD", "Mitohondrion", "Vesicles"], ["Mitohondrion", "Vesicles"]]
+            "PSD" can be only in the first list
+
+        '''
+        if split:
+            element_dict1 = {}
+            element_dict2 = {}
+            for key in element_dict:
+                if key != "PSD":
+                    element_dict1[key] = np.random.randint(0, element_dict[key] + 1)
+                    element_dict2[key] = element_dict[key] - element_dict1[key]
+            element_dict1["PSD"] = element_dict["PSD"]
+             
+            element_list1 = []
+            for key in element_dict1:
+                element_list1.extend([key] * element_dict1[key]) 
+            element_list2 = []
+            for key in element_dict2:
+                element_list1.extend([key] * element_dict2[key]) 
+            random.shuffle(element_list1)    
+            random.shuffle(element_list2)    
+            return [element_list1, element_list2]
+        else:
+            element_list = []
+            for key in element_dict:
+                element_list.extend([key] * element_dict[key])
+            random.shuffle(element_list)  
+            return [element_list]
+            
 
     def createListGeneration(self, max_count_PSD = 0, max_count_Axon = 0, max_count_Vesicles = 0, max_count_Mitohondrion = 0, spam = 0):
 
-        if max_count_PSD > 0:
-            count_PSD = np.random.randint(1, max_count_PSD+1)
-        else:
-            count_PSD = 0
-
-        if max_count_Axon > 0:
-            count_Axon = np.random.randint(1, max_count_Axon+1)
-        else:
-            count_Axon = 0
-
-        if max_count_Vesicles > 0:
-            count_Vesicles = np.random.randint(1, max_count_Vesicles+1)
-        else:
-            count_Vesicles = 0
-
-        if max_count_Mitohondrion > 0:
-            count_Mitohondrion = np.random.randint(1, max_count_Mitohondrion+1)
-        else:
-            count_Mitohondrion = 0
-
-
-        addAxonAfterMempbran         = np.random.randint(0, count_Axon+1)
-        addMitohondrionAfterMempbran = np.random.randint(0, count_Mitohondrion+1)
-        addVesiclesAfterMempbran     = np.random.randint(0, count_Vesicles+1)
-
-        addAxonBeforeMempbran         = count_Axon         - addAxonAfterMempbran
-        addMitohondrionBeforeMempbran = count_Mitohondrion - addMitohondrionAfterMempbran
-        addVesiclesBeforeMempbran     = count_Vesicles     - addVesiclesAfterMempbran
-
-        RetList = []
-
-        ListMixingClass = [["Axon"        , addAxonBeforeMempbran],
-                           ["Mitohondrion", addMitohondrionBeforeMempbran],
-                           ["Vesicles"    , addVesiclesBeforeMempbran],
-                           ["PSD"         , count_PSD]]
-
-        while len(ListMixingClass) != 0:
-            index = np.random.randint(0, len(ListMixingClass))
-
-            if ListMixingClass[index][1] <= 0:
-                ListMixingClass.pop(index)
-            else:
-                ListMixingClass[index][1] -= 1
-
-                if ListMixingClass[index][0] == "Axon":
-                    newElement = Axon()
-                elif ListMixingClass[index][0] == "Mitohondrion":
-                    newElement = Mitohondrion()
-                elif ListMixingClass[index][0] == "Vesicles":
-                    newElement = Vesicles()
-                elif ListMixingClass[index][0] == "PSD":
-                    newElement = PSD()
+        RetList = []        
+        
+        count_PSD = self.get_count(max_count_PSD)
+        count_Axon = self.get_count(max_count_Axon)
+        count_Vesicles = self.get_count(max_count_Vesicles)
+        count_Mitohondrion = self.get_count(max_count_Mitohondrion)  
+        
+        element_dict = {"Axon": count_Axon, "Mitohondrion" : count_Mitohondrion, 
+                        "Vesicles": count_Vesicles, "PSD" : count_PSD}
+        
+        element_list = self.generate_lists(element_dict)
+        
+        try:
+            for element in element_list[0]:
+                if element in self.class_constructors:
+                    newElement = self.class_constructors[element]()
+                    UnionMask = self.addNewElementIntoImage(RetList, newElement)
                 else:
-                    raise Exception("What's up ?")
-
-                UnionMask = self.addNewElementIntoImage(RetList, newElement)
-
-        #################################################
-        UnionMask = self.addNewElementIntoImage(RetList, Membrane(self.sizeImage, RetList))
-        #################################################
-
-        ListMixingClass2 = [["Axon"        , addAxonAfterMempbran],
-                            ["Mitohondrion", addMitohondrionAfterMempbran],
-                            ["Vesicles"    , addVesiclesAfterMempbran]]
-
-        while len(ListMixingClass2) != 0:
-            index = np.random.randint(0, len(ListMixingClass2))
-
-            if ListMixingClass2[index][1] <= 0:
-                ListMixingClass2.pop(index)
-            else:
-                ListMixingClass2[index][1] -= 1
-                try:
-                    if ListMixingClass2[index][0] == "Axon":
-                        newElement = Axon()
-                    elif ListMixingClass2[index][0] == "Mitohondrion":
-                        newElement = Mitohondrion()
-                    elif ListMixingClass2[index][0] == "Vesicles":
-                        newElement = Vesicles()
-                    else:
-                        raise Exception("What's up ?")
-
-                    self.addNewElementIntoImage(RetList, newElement)
-                except Exception as ex:
-                    print(ex)
-                    #print(f"{type(ex).__name__} at line {ex.__traceback__.tb_lineno} of {__file__}: {ex}")
-
+                    raise Exception("No such class exist: " + element)
+    
+            # DRAW MEMBRANES
+            UnionMask = self.addNewElementIntoImage(RetList, Membrane(self.sizeImage, RetList))
+        
+            for element in element_list[1]:
+                if element in self.class_constructors:
+                    newElement = self.class_constructors[element]()
+                    UnionMask = self.addNewElementIntoImage(RetList, newElement) 
+                else:
+                    raise Exception("No such class exist: " + element)
+        except Exception as ex:
+            print(ex)
+            
         if spam != 0:
             RetList.append(SpamComponents(RetList.copy(), spam))
 
         return RetList
 
     def createListGenerationWithMask(self, max_count_PSD = 0, max_count_Axon = 0, max_count_Vesicles = 0, max_count_Mitohondrion = 0):
-
-        if max_count_PSD > 0:
-            count_PSD = np.random.randint(1, max_count_PSD+1)
-        else:
-            count_PSD = 0
-
-        if max_count_Axon > 0:
-            count_Axon = np.random.randint(1, max_count_Axon+1)
-        else:
-            count_Axon = 0
-
-        if max_count_Vesicles > 0:
-            count_Vesicles = np.random.randint(1, max_count_Vesicles+1)
-        else:
-            count_Vesicles = 0
-
-        if max_count_Mitohondrion > 0:
-            count_Mitohondrion = np.random.randint(1, max_count_Mitohondrion+1)
-        else:
-            count_Mitohondrion = 0
-
-
-        addAxonAfterMempbran         = np.random.randint(0, count_Axon+1)
-        addMitohondrionAfterMempbran = np.random.randint(0, count_Mitohondrion+1)
-        addVesiclesAfterMempbran     = np.random.randint(0, count_Vesicles+1)
-
-        addAxonBeforeMempbran         = count_Axon         - addAxonAfterMempbran
-        addMitohondrionBeforeMempbran = count_Mitohondrion - addMitohondrionAfterMempbran
-        addVesiclesBeforeMempbran     = count_Vesicles     - addVesiclesAfterMempbran
-
         RetList = []
+        
+        count_PSD = self.get_count(max_count_PSD)
+        count_Axon = self.get_count(max_count_Axon)
+        count_Vesicles = self.get_count(max_count_Vesicles)
+        count_Mitohondrion = self.get_count(max_count_Mitohondrion)  
 
-        ListMixingClass = [["Axon"        , addAxonBeforeMempbran],
-                           ["Mitohondrion", addMitohondrionBeforeMempbran],
-                           ["Vesicles"    , addVesiclesBeforeMempbran],
-                           ["PSD"         , count_PSD]]
+        element_dict = {"Axon": count_Axon, "Mitohondrion" : count_Mitohondrion, 
+                        "Vesicles": count_Vesicles, "PSD" : count_PSD}
+        
+        element_list = self.generate_lists(element_dict)
 
         UnionMask = np.zeros((*self.sizeImage,3), np.uint8)
-
-        while len(ListMixingClass) != 0:
-            index = np.random.randint(0, len(ListMixingClass))
-
-            if ListMixingClass[index][1] <= 0:
-                ListMixingClass.pop(index)
-            else:
-                ListMixingClass[index][1] -= 1
-
-                if ListMixingClass[index][0] == "Axon":
-                    newElement = Axon()
-                elif ListMixingClass[index][0] == "Mitohondrion":
-                    newElement = Mitohondrion()
-                elif ListMixingClass[index][0] == "Vesicles":
-                    newElement = Vesicles()
-                elif ListMixingClass[index][0] == "PSD":
-                    newElement = PSD()
-                else:
-                    raise Exception("What's up ?")
-
-                UnionMask = self.addNewElementIntoImage_WithMask(RetList, UnionMask, newElement)
-
-        #cv2.imshow("testMask", UnionMask)
-        #cv2.waitKey()
-        #################################################
-        UnionMask = self.addNewElementIntoImage_WithMask(RetList, UnionMask, Membrane(self.sizeImage, RetList))
-        #################################################
-
-        ListMixingClass2 = [["Axon"        , addAxonAfterMempbran],
-                            ["Mitohondrion", addMitohondrionAfterMempbran],
-                            ["Vesicles"    , addVesiclesAfterMempbran]]
-
-        while len(ListMixingClass2) != 0:
-            index = np.random.randint(0, len(ListMixingClass2))
-
-            if ListMixingClass2[index][1] <= 0:
-                ListMixingClass2.pop(index)
-            else:
-                ListMixingClass2[index][1] -= 1
-                try:
-                    if ListMixingClass2[index][0] == "Axon":
-                        newElement = Axon()
-                    elif ListMixingClass2[index][0] == "Mitohondrion":
-                        newElement = Mitohondrion()
-                    elif ListMixingClass2[index][0] == "Vesicles":
-                        newElement = Vesicles()
-                    else:
-                        raise Exception("What's up ?")
-
-                    UnionMask = self.addNewElementIntoImage_WithMask(RetList, UnionMask, newElement)
-                except Exception as ex:
-                    print(ex)
-                    #print(f"{type(ex).__name__} at line {ex.__traceback__.tb_lineno} of {__file__}: {ex}")
-
+        
+        try:
+            for element in element_list[0]:
+               if element in self.class_constructors:
+                   newElement = self.class_constructors[element]()
+                   UnionMask = self.addNewElementIntoImage_WithMask(RetList, UnionMask, newElement)
+               else:
+                   raise Exception("No such class exist: " + element)
+            
+            #cv2.imshow("testMask", UnionMask)
+            #cv2.waitKey()
+            # DRAW MEMBRANES
+            UnionMask = self.addNewElementIntoImage_WithMask(RetList, UnionMask, Membrane(self.sizeImage, RetList))
+    
+            for element in element_list[1]:
+               if element in self.class_constructors:
+                   newElement = self.class_constructors[element]()
+                   UnionMask = self.addNewElementIntoImage_WithMask(RetList, UnionMask, newElement)
+               else:
+                   print("No such class exist: " + element)
+                   raise Exception("No such class exist: " + element)
+        except Exception as ex:
+            print(ex)
+            
         return RetList
 
     def createListGenerationWithStartMembrane(self, max_count_PSD = 0, max_count_Axon = 0, max_count_Vesicles = 0, max_count_Mitohondrion = 0):
-        if max_count_PSD > 0:
-            count_PSD = np.random.randint(1, max_count_PSD+1)
-        else:
-            count_PSD = 0
-
-        if max_count_Axon > 0:
-            count_Axon = np.random.randint(1, max_count_Axon+1)
-        else:
-            count_Axon = 0
-
-        if max_count_Vesicles > 0:
-            count_Vesicles = np.random.randint(1, max_count_Vesicles+1)
-        else:
-            count_Vesicles = 0
-
-        if max_count_Mitohondrion > 0:
-            count_Mitohondrion = np.random.randint(1, max_count_Mitohondrion+1)
-        else:
-            count_Mitohondrion = 0
-
+        
         RetList = []
+        
+        count_PSD = self.get_count(max_count_PSD)
+        count_Axon = self.get_count(max_count_Axon)
+        count_Vesicles = self.get_count(max_count_Vesicles)
+        count_Mitohondrion = self.get_count(max_count_Mitohondrion)  
 
-        ListMixingClass = [["Axon"        , count_Axon],
-                           ["Mitohondrion", count_Mitohondrion],
-                           ["Vesicles"    , count_Vesicles],
-                           ["PSD"         , count_PSD]]
+        element_dict = {"Axon": count_Axon, "Mitohondrion" : count_Mitohondrion, 
+                         "Vesicles": count_Vesicles, "PSD" : count_PSD}
+         
+        element_list = self.generate_lists(element_dict, False)
 
-        #################################################
+        # DRAW MEMBRANES
         UnionMask = self.addNewElementIntoImage(RetList, Membrane(self.sizeImage))
-        #################################################
 
-        while len(ListMixingClass) != 0:
-            index = np.random.randint(0, len(ListMixingClass))
-
-            if ListMixingClass[index][1] <= 0:
-                ListMixingClass.pop(index)
-            else:
-                ListMixingClass[index][1] -= 1
-
-                if ListMixingClass[index][0] == "Axon":
-                    newElement = Axon()
-                elif ListMixingClass[index][0] == "Mitohondrion":
-                    newElement = Mitohondrion()
-                elif ListMixingClass[index][0] == "Vesicles":
-                    newElement = Vesicles()
-                elif ListMixingClass[index][0] == "PSD":
-                    newElement = PSD()
+        try:
+            for element in element_list[0]:
+                if element in self.class_constructors:
+                    newElement = self.class_constructors[element]()
+                    UnionMask = self.addNewElementIntoImage(RetList, newElement)
                 else:
-                    raise Exception("What's up ?")
-
-                UnionMask = self.addNewElementIntoImage(RetList, newElement)
-
+                    raise Exception("No such class exist: " + element)
+        except Exception as ex:
+            print(ex)
+            
         return RetList
 
     def fake_3_layers(self, ListGeneration, counter, dir_save, startIndex, size_overlap = 5):
